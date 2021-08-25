f94e4a40f439e8ed94aa49e8b9cb199efb48c465
Sadiq O <sadiqomar890@gmail.com>
2021-08-25 21:10:24 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p>my name is {{ name }} and my age is {{ age }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    let name = "sadiq";
+    let age = "19";
+
+    return { name, age };
+  },
+};
 </script>
