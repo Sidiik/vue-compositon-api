55d54e51cfb4044a13ed937ba3a39c494e4146fc
Sadiq O <sadiqomar890@gmail.com>
2021-08-26 23:07:50 +0300

    modified

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,7 +1,31 @@
-<template> </template>
+<template>
+  <div class="post">
+    <h1>
+      {{ post.title }}
+    </h1>
+    <p>{{ snippet }}</p>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  props: ["post"],
+  setup(props) {
+    const snippet = props.post.body.substring(0, 90) + " ...";
+    return { snippet };
+  },
+};
 </script>
 
-<style></style>
+<style>
+.post {
+  text-align: left;
+  width: 60%;
+  margin: auto;
+  background: rgb(22, 214, 124);
+  padding: 1rem;
+  margin-bottom: 1rem;
+  border-radius: 0.3rem;
+  color: white;
+}
+</style>
--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,10 +1,14 @@
 <template>
-  <div v-for="post in posts" :key="post.id"></div>
+  <div v-for="post in posts" :key="post.id">
+    <PostItem :post="post" />
+  </div>
 </template>
 
 <script>
+import PostItem from "./PostItem.vue";
 export default {
   props: ["posts"],
+  components: { PostItem },
 };
 </script>
 
